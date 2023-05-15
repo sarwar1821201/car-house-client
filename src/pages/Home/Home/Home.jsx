b27caea90131baf1afbdc4dbfa257599eaab0cbf
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-05-16 01:21:02 +0600

    create about section with image

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 
 
@@ -5,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2>This is Home</h2>
+            <About></About>
         </div>
     );
 };
