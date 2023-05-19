693d28a8984cb98700d57eb8cf971a82320081ec
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-05-19 23:58:16 +0600

    confirm the booking session

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,8 @@ import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
 
 import BookService from "../pages/BookService/BookService";
+import Bookings from "../pages/Bookings/Bookings";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -27,6 +29,10 @@ const router = createBrowserRouter([
             path: 'book/:id',
             element: <BookService></BookService>,
             loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+        },
+        {
+          path: 'bookings',
+          element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
         }
        
       ]
